978fb5f92760ab483375e0d790e48a4a8fbb4742
jjuuuunnii <jake991110@naver.com>
2024-10-27 20:49:36 +0900

    Setting: Spring 기본 세팅

---
+++ src/main/java/com/inglo/giggle/GiggleNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.inglo.giggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiggleNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiggleNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inglo/giggle/GiggleNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inglo.giggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiggleNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
